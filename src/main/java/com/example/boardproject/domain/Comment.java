bc66378259c9464b0cae9758a9fd446c7890226f
JunHyeongChoi <ajfeh22@gmail.com>
2023-02-12 06:51:21 +0900

    #10 - 도메인 설계
    게시글과 댓글의 도메인을 설계하였다.
    
    연관관계를 적절히 고려해 `@ManyToOne` , `@OneToMany` 를 작성해주었다.

---
+++ src/main/java/com/example/boardproject/domain/Comment.java
@@ -0,0 +1,34 @@
+package com.example.boardproject.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    public Comment() {
+    }
+
+    @Builder
+    public Comment(String content) {
+        this.content = content;
+    }
+
+}
