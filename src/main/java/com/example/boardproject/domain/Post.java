a62844ae1403c9ab7d80d67c48414cdb9880f26a
JunHyeongChoi <ajfeh22@gmail.com>
2023-02-13 03:27:14 +0900

    #12 - id 타입을 Long으로 변경
    원래 이전에 했던 작업인데 알 수 없는 이유로 원상복구 되어 다시 수정해주었다.

--- src/main/java/com/example/boardproject/domain/Post.java
+++ src/main/java/com/example/boardproject/domain/Post.java
@@ -2,23 +2,28 @@ package com.example.boardproject.domain;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@NoArgsConstructor
 @Getter
 @Entity
 public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
+    @Setter
     @Column(length = 200, nullable = false)
     private String title;
 
+    @Setter
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
 
@@ -27,13 +32,11 @@ public class Post {
     @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE)
     private List<Comment> comments = new ArrayList<>();
 
-    public Post() {
-    }
-
     @Builder
-    public Post(String title, String content) {
+    public Post(String title, String content, LocalDateTime createDate) {
         this.title = title;
         this.content = content;
+        this.createDate = createDate;
     }
 
 }
