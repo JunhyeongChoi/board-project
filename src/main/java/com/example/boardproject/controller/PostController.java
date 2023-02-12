b6036c37410a2cce3e33e8e3679903c8df8906cc
JunHyeongChoi <ajfeh22@gmail.com>
2023-02-13 03:30:31 +0900

    #12 - 게시글 CRUD 처리
    게시글 CRUD를 위해 컨트롤러, 리파지토리, 서비스를 작성해주었다.

---
+++ src/main/java/com/example/boardproject/controller/PostController.java
@@ -0,0 +1,60 @@
+package com.example.boardproject.controller;
+
+import com.example.boardproject.dto.PostDto;
+import com.example.boardproject.dto.request.PostSaveRequest;
+import com.example.boardproject.dto.request.PostUpdateRequest;
+import com.example.boardproject.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/posts")
+@RequiredArgsConstructor
+@Controller
+public class PostController {
+
+    private final PostService postService;
+
+    // 게시판 조회
+    @GetMapping
+    String posts(Model model) {
+        List<PostDto> posts = postService.getPosts();
+
+        model.addAttribute("posts", posts);
+        return "board";
+    }
+
+    // 게시글 조회
+    @GetMapping("{postId}")
+    String post(@PathVariable Long postId, Model model) {
+        PostDto post = postService.getPost(postId);
+
+        model.addAttribute("post", post);
+        return "post";
+    }
+
+    // 게시글 작성
+    @ResponseBody
+    @PostMapping
+    void create(@RequestBody PostSaveRequest postSaveRequest) {
+        postService.savePost(postSaveRequest);
+    }
+
+    // 게시글 수정
+    @ResponseBody
+    @PutMapping("{postId}")
+    void update(@PathVariable Long postId, @RequestBody PostUpdateRequest postUpdateRequest) {
+        postService.updatePost(postId, postUpdateRequest);
+    }
+
+    // 게시글 삭제
+    @ResponseBody
+    @DeleteMapping("{postId}")
+    void delete(@PathVariable Long postId) {
+        postService.deletePost(postId);
+    }
+
+}
---
+++ src/main/java/com/example/boardproject/repository/PostRepository.java
@@ -0,0 +1,8 @@
+package com.example.boardproject.repository;
+
+import com.example.boardproject.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
