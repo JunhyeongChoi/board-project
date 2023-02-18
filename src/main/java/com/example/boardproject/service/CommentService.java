f94aa51eaf670356703b19827ab329e2775643f2
JunHyeongChoi <ajfeh22@gmail.com>
2023-02-18 20:20:21 +0900

    #20 - `@Transactional` 추가

--- src/main/java/com/example/boardproject/service/CommentService.java
+++ src/main/java/com/example/boardproject/service/CommentService.java
@@ -6,10 +6,12 @@ import com.example.boardproject.dto.PostDto;
 import com.example.boardproject.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class CommentService {
 
