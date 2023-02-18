2160e7dff020fa8bb955d5112802c58af7f4ba19
JunHyeongChoi <ajfeh22@gmail.com>
2023-02-19 02:16:05 +0900

    #22 - 시큐리티 설정
    우선 모든 요청에서 로그인이 필요 없도록 설정해두었다.

---
+++ src/main/java/com/example/boardproject/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.boardproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity  // 모든 요청이 시큐리티의 제어를 받도록 한다.
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().requestMatchers(
+                new AntPathRequestMatcher("/**")).permitAll()
+        ;
+        return http.build();
+    }
+
+}
