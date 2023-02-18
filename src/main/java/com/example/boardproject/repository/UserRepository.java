a7ceb237abd17bc62b1455a50c19a6a1367a639d
JunHyeongChoi <ajfeh22@gmail.com>
2023-02-19 03:41:12 +0900

    #24 - 로그인 기능 구현

--- src/main/java/com/example/boardproject/repository/UserRepository.java
+++ src/main/java/com/example/boardproject/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package com.example.boardproject.repository;
 
-import com.example.boardproject.domain.User;
+import com.example.boardproject.domain.user.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByUsername(String username);
+
 }
