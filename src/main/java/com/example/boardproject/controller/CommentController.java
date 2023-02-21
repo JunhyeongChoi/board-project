bb9386c84188144a84be3984f1504913980a3fa7
JunHyeongChoi <ajfeh22@gmail.com>
2023-02-22 04:31:22 +0900

    #28 - 댓글 수정 및 삭제 구현

--- src/main/java/com/example/boardproject/controller/CommentController.java
+++ src/main/java/com/example/boardproject/controller/CommentController.java
@@ -1,19 +1,24 @@
 package com.example.boardproject.controller;
 
+import com.example.boardproject.domain.Comment;
 import com.example.boardproject.domain.Post;
 import com.example.boardproject.domain.user.Member;
 import com.example.boardproject.dto.request.CommentRequestDto;
+import com.example.boardproject.dto.request.PostRequestDto;
 import com.example.boardproject.service.CommentService;
 import com.example.boardproject.service.PostService;
 import com.example.boardproject.service.user.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.security.Principal;
@@ -47,6 +52,54 @@ public class CommentController {
 
         commentService.saveComment(post, member, commentRequestDto.getContent());
         return "redirect:/post/" + id;
+    }
+
+    // 댓글 수정
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String CommentModify(Model model, @PathVariable Long id, Principal principal) {
+
+        Comment comment = commentService.getComment(id);
+        // 수정페이지 들어갔을 때 내용이 그대로 채워져 있게끔 할 때 사용
+        CommentRequestDto commentRequestDto = CommentRequestDto.toDto(comment);
+
+        if(!principal.getName().equals(comment.getMember().getUsername())) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "수정 권한이 없습니다.");
+        }
+
+        model.addAttribute(commentRequestDto);
+        return "comment_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String commentModify(@Valid CommentRequestDto commentRequestDto, BindingResult bindingResult,
+                             Principal principal, @PathVariable Long id) {
+        if (bindingResult.hasErrors()) {
+            return "comment_form";
+        }
+
+        Comment comment = commentService.getComment(id);
+        if (!principal.getName().equals(comment.getMember().getUsername())) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "수정 권한이 없습니다.");
+        }
+
+        commentService.updateComment(comment, commentRequestDto);
+        return "redirect:/post/" + comment.getPost().getId();
+    }
+
+    // 댓글 삭제
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String commentDelete(Principal principal, @PathVariable Long id) {
+        Comment comment = commentService.getComment(id);
+
+        if (!principal.getName().equals(comment.getMember().getUsername())) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "삭제 권한이 없습니다.");
+        }
+
+        commentService.deleteComment(comment);
+        return "redirect:/post/" + comment.getPost().getId();
     }
 
 }
--- src/main/java/com/example/boardproject/service/CommentService.java
+++ src/main/java/com/example/boardproject/service/CommentService.java
@@ -3,11 +3,14 @@ package com.example.boardproject.service;
 import com.example.boardproject.domain.Comment;
 import com.example.boardproject.domain.Post;
 import com.example.boardproject.domain.user.Member;
+import com.example.boardproject.dto.request.CommentRequestDto;
+import com.example.boardproject.dto.request.PostRequestDto;
 import com.example.boardproject.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
@@ -26,6 +29,27 @@ public class CommentService {
                 .build();
 
         commentRepository.save(comment);
+    }
+
+    // 댓글 조회
+    @Transactional(readOnly = true)
+    public Comment getComment(Long postId) {
+        return commentRepository.findById(postId)
+                .orElseThrow(() -> new EntityNotFoundException("댓글을 찾을 수 없습니다."));
+    }
+
+    // 댓글 수정
+    public void updateComment(Comment comment, CommentRequestDto commentRequestDto) {
+        if (comment.getContent() == null)
+            return;
+
+        comment.setContent(commentRequestDto.getContent());
+        comment.setModifyDate(LocalDateTime.now());
+    }
+
+    // 댓글 삭제
+    public void deleteComment(Comment comment) {
+        commentRepository.delete(comment);
     }
 
 }
