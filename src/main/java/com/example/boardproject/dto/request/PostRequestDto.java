36d1e3bafcc57a10be344b01d531479abcee9db3
JunHyeongChoi <ajfeh22@gmail.com>
2023-02-22 04:32:28 +0900

    #28 - `@NotEmpty` 를 `@NotBlank` 로 변경
    두 어노테이션의 차이는 `@NotEmpty` 는 공백을 허용하지만 `@NotBlank` 는 공백을 허용하지 않는다.

--- src/main/java/com/example/boardproject/dto/request/PostRequestDto.java
+++ src/main/java/com/example/boardproject/dto/request/PostRequestDto.java
@@ -6,18 +6,18 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Getter @Setter
 public class PostRequestDto {
 
-    @NotEmpty(message="제목은 필수 항목입니다.")
+    @NotBlank(message="제목은 필수 항목입니다.")
     @Size(max=200)
     private String title;
 
-    @NotEmpty(message="내용은 필수 항목입니다.")
+    @NotBlank(message="내용은 필수 항목입니다.")
     private String content;
 
     private Member member;
