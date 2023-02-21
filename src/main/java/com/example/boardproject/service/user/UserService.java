091958634806cb281a0133d3cb1a3335c6604256
JunHyeongChoi <ajfeh22@gmail.com>
2023-02-21 23:28:21 +0900

    #26 - 회원을 얻어오는 메서드 추가
    회원을 이름으로 얻어오는 메서드를 추가했다.

--- src/main/java/com/example/boardproject/service/user/UserService.java
+++ src/main/java/com/example/boardproject/service/user/UserService.java
@@ -6,6 +6,9 @@ import com.example.boardproject.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Service
 public class UserService {
@@ -19,4 +22,14 @@ public class UserService {
         return member;
     }
 
+    public Member getMember(String username) {
+        Optional<Member> member = userRepository.findByUsername(username);
+
+        if (member.isPresent()) {
+            return member.get();
+        } else {
+            throw new EntityNotFoundException("유저를 찾을 수 없습니다.");
+        }
+    }
+    
 }
