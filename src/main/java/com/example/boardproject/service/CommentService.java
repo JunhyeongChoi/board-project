a30730f3ea56cf5dd35eb032d818c6cf64a3e7d6
JunHyeongChoi <ajfeh22@gmail.com>
2023-02-22 23:26:47 +0900

    #34 - 대댓글 추가로 댓글 개수 표시 수정
    대댓글이 추가되어 기존 댓글 개수 표시에 대댓글을 더한 값을 표시하도록 수정하였다.

--- src/main/java/com/example/boardproject/service/CommentService.java
+++ src/main/java/com/example/boardproject/service/CommentService.java
@@ -5,18 +5,21 @@ import com.example.boardproject.domain.Post;
 import com.example.boardproject.domain.user.Member;
 import com.example.boardproject.dto.request.CommentRequestDto;
 import com.example.boardproject.repository.CommentRepository;
+import com.example.boardproject.repository.ReplyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Transactional
 @Service
 public class CommentService {
 
+    private final PostService postService;
     private final CommentRepository commentRepository;
 
     // 댓글 저장
@@ -48,6 +51,22 @@ public class CommentService {
     // 댓글 삭제
     public void deleteComment(Comment comment) {
         commentRepository.delete(comment);
+    }
+
+    // 해당 게시글 전체 댓글 개수 조회
+    public int getCount(Long id) {
+        Post post = postService.getPost(id);
+
+        int size = 0;
+
+        List<Comment> comments = post.getComments();
+        size += comments.size();
+
+        for (Comment comment : comments) {
+            size += comment.getReplies().size();
+        }
+
+        return size;
     }
 
 }
